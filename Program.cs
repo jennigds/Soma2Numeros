24541a08c91484a248df73ca18495956e7236b62
Jennigds <jennifergds89@gmail.com>
2023-04-20 22:35:31 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int n1, n2, n3;
+
+Console.WriteLine("Escreva para calcular o número.\n");
+
+Console.Write("Digite um número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.Write("Digite outro número: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+n3 = n1 + n2;
+
+Console.WriteLine();
+Console.Write($"Resultado: {n3}");
+
